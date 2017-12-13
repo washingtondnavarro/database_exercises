be5ffbe39453ebb41576debc45d3040c6752ce10
washingtondnavarro <washington.d.navarro@gmail.com>
2017-12-13 10:40:48 -0600

    adding delete exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,16 @@
+use codeup_test_db;
+
+# SELECT * from albums where release_date > 1991;
+# DELETE from albums where release_date > 1991;
+# SELECT * from albums where release_date > 1991;
+
+# select * from albums where genre = 'disco';
+# DELETE from albums where genre = 'disco';
+# select * from albums where genre = 'disco';
+#
+# SELECT * from albums where artist_name = 'whitney houston';
+# DELETE from albums where artist_name = 'whitney houston';
+# SELECT * from albums where artist_name = 'whitney houston';
+#
+
+
