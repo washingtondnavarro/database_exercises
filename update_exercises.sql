5348e0064f85fa77f0a1e197a2bd99ff77a52f88
washingtondnavarro <washington.d.navarro@gmail.com>
2017-12-13 11:07:01 -0600

    adding update exercise

---
+++ update_exercises.sql
@@ -0,0 +1,30 @@
+use codeup_test_db;
+
+# SELECT * FROM codeup_test_db.albums;
+#
+# UPDATE albums set copies_cert_sold = copies_cert_sold * 10;
+#
+# SELECT * FROM codeup_test_db.albums;
+
+
+
+# SELECT * FROM albums WHERE release_date < 1980;
+#
+# UPDATE albums set release_date = 1800 WHERE release_date < 1980;
+#
+# SELECT * FROM albums WHERE release_date < 1980;
+#
+#
+#
+#
+# SELECT * FROM albums WHERE artist_name = 'michael Jackson';
+#
+# UPDATE albums set artist_name = 'peter jackson' where artist_name = 'michael jackson';
+#
+# SELECT * FROM albums WHERE artist_name = 'michael Jackson';
+#
+SELECT * FROM albums WHERE artist_name = 'peter Jackson';
+
+
+
+
