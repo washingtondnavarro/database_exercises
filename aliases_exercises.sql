411081c8f450604ba77e6a01e090a15fa5a2e7a4
washingtondnavarro <washington.d.navarro@gmail.com>
2017-12-14 11:39:22 -0600

    update the structure of the file

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -5,4 +5,8 @@ USE employees;
 # SELECT concat(last_name,' ', first_name) AS 'full_name', birth_date AS 'DOB' FROM employees limit 10;
 
 
-# SELECT concat(emp_no,' - ',last_name,' ',first_name) AS 'full_name', birth_date AS 'DOB' FROM employees limit 10;
+SELECT
+  concat(emp_no, ' - ', last_name, ' ', first_name) AS 'full_name',
+  birth_date                                        AS 'DOB'
+FROM employees
+LIMIT 10;
